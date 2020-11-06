3a35ed669b6aa74dbdbba987a9e5c5dac1caaf44
zharuiqi <51909822+zharuiqi@users.noreply.github.com>
2020-11-06 22:17:32 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+import allure
+
+@allure.feature("智能调度台操作")
+class TestLogin:
+
+    def test_login_001(self):
+        assert 0
+
+    def test_login_002(self):
+        assert 1
+
+    def test_login_003(self):
+        assert 1
